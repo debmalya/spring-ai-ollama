027e4b7ff4187973f61b83a4d346cf472d6fcacd
Debmalya Jash <gkedeb@gmail.com>
2025-07-29 20:37:09 -0400

    Intital (working) commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# About
+This will run spring-ai application with locally installed ollama.
+# Prerequisites
+* From [Ollama](https://ollama.com) download installer for your OS.
+* Follow installation instruction.
---
+++ src/main/java/com/dj/spring/boot/ollama/local/Application.java
@@ -0,0 +1,13 @@
+package com.dj.spring.boot.ollama.local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
